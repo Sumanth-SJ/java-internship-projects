4425d0343b5fbc0b3c4e9a066c417c21cc315b85
Sumanth-SJ <sumanthsj74@gmail.com>
2026-02-16 18:43:24 +0530

    Create Customers.java

---
+++ Week 5/src/Customers/Customers.java
@@ -0,0 +1,22 @@
+package com.exm.customers;
+
+
+
+public class Customer {
+
+    private String customerId;
+    private String name;
+    private String email;
+
+    public Customer(String customerId, String name, String email) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public void displayCustomer() {
+        System.out.println("Customer ID: " + customerId);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+    }
+}
